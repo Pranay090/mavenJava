6127bff0eaaf0d89c6935e7679ec507c91580868
Pranay090 <138748728+Pranay090@users.noreply.github.com>
2024-11-25 11:40:15 +0530

    Add files via upload

---
+++ src/main/java/com/mavenapp/SE_LAB/App.java
@@ -0,0 +1,10 @@
+package com.mavenapp.SE_LAB;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mavenapp/SE_LAB/AppTest.java
@@ -0,0 +1,19 @@
+package com.mavenapp.SE_LAB;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
